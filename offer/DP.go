2ec9812e7fbe3a626978d0488b2a5da508b33885
Doris <64065933@qq.com>
2021-03-03 16:31:56 +0800

    增加dp的说明

--- offer/DP.go
+++ offer/DP.go
@@ -13,3 +13,42 @@ func numWays(n int) int {
 	return dp[n]
 
 }
+
+//优化
+func numWays1(n int) int {
+	a, b := 1, 1
+	for i := 1; i <= n; i++ {
+		a, b = b, (a+b)%1000000007
+	}
+	return a
+}
+
+func maxValue(grid [][]int) int {
+	if len(grid) == 0 {
+		return 0
+	}
+	m, n := len(grid), len(grid[0])
+	dp := make([]int, n)
+	dp[0] = 1
+	for j := 1; j < n; j++ {
+		dp[j] = grid[0][j]
+	}
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if j == 0 {
+				dp[j] += grid[i][j]
+			} else {
+				dp[j] = max(dp[j], dp[j-1]) + grid[i][j]
+			}
+		}
+	}
+	return dp[n-1]
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
