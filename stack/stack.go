38aa455bcffbfb207973a4d337e5a0429dcc7c41
Doris <64065933@qq.com>
2021-03-09 12:04:55 +0800

    增加了栈的思考

--- stack/stack.go
+++ stack/stack.go
@@ -1,1 +1,13 @@
 package stack
+
+func removeDuplicates(S string) string {
+	res := []byte{}
+	for i := range S {
+		if len(res) != 0 && res[len(res)-1] == S[i] {
+			res = res[:len(res)-1]
+		} else {
+			res = append(res, S[i])
+		}
+	}
+	return string(res)
+}
